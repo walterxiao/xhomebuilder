d24c314a90d4c8275cec2a8a11471e29ad371c11
walterxiao <walter.siu@gmail.com>
2026-02-21 14:11:52 -0500

    Initial React Tic Tac Toe skeleton.

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export default function App() {
+const [cells, setSquare] = useState([]);
+/* --sample react component stubbed out TBD */
+return (<div></div>)
+}

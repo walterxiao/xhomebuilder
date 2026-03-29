35f987a69b7d15360499759bfa268ab13b43df0a
Walter Siu <walter.siu@gmail.com>
2026-03-29 00:01:23 -0400

    update port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const battleship = require('./games/battleship');
 const pictionary = require('./games/pictionary');
 const presence = require('./games/presence');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9753;
 
 const PAGES = {
   '/':           'index.html',
